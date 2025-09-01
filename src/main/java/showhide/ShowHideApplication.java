09506ab60480d233608404992e643682ece0945e
malekz2 <basaltaltalt@gmail.com>
2025-09-01 11:17:24 +0400

    created showhide project

---
+++ src/main/java/showhide/ShowHideApplication.java
@@ -0,0 +1,19 @@
+package showhide;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author malekzuhdi on 01/09/2025
+ */
+public class ShowHideApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
